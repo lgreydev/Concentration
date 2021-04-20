60f343a7effd6ecc9e9a50a7dfa05c8fc33b5726
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-04-20 19:43:13 +0300

    Add flip card

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    
+    
+    
+    // MARK: - IBAction
+    
+    /// Call a function that flips the card
+    /// - Parameter sender: The game card
     @IBAction func touchCard(_ sender: UIButton) {
-        print(#line)
+        print(#line, #function)
+        flipCard(withEmoji: "🧟‍♂️", on: sender)
     }
     
+    // MARK: - Methods
+    
+    /// Flip the card face down if emoji match
+    /// - Parameters:
+    ///   - emoji: The element that is compared to the title of the button
+    ///   - button: The game map in which we make changes
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            print(#line)
+            button.setTitle("", for: .normal)
+            button.backgroundColor = .orange
+        } else {
+            button.backgroundColor = .white
+            button.setTitle(emoji, for: .normal)
+            print(#line)
+        }
+    }
 }
 
