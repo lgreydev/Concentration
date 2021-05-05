b3190d1c762ef89fd5d6d9b2dc7e19140a47f6c3
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-05-05 22:11:03 +0300

    Make 'indexOfOneAndOnlyFaceUpCard'​ computed property

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -12,7 +12,29 @@ class Concentration {
     
     // MARK: - Variables
     var cards = [Card]()
-    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+            
+        }
+    }
     
     
     // MARK: - Methods
@@ -24,13 +46,7 @@ class Concentration {
                     cards[index].isMatched = true
                 }
                 cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = nil
             } else {
-                // either no cards or 2 cards are face up
-                for flipdownIndex in cards.indices {
-                    cards[flipdownIndex].isFaceUp = false
-                }
-                cards[index].isFaceUp = true
                 indexOfOneAndOnlyFaceUpCard = index
             }
         }
