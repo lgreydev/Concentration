9be4193fc715708deeac1e29ea6f20aa3fdd16e0
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-12-04 12:51:32 +0200

    refactor: rename view controller

--- Concentration/ConcentrationViewController.swift
+++ Concentration/ConcentrationViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConcentrationViewController: UIViewController {
     
     // MARK: IBOutlets
     /// The array of cards that we get from the View
